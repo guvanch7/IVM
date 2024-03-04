3fc9aee33b1d72a79596b94c1209e63ec9a6ff81
guvanch7 <135972158+guvanch7@users.noreply.github.com>
2024-03-04 05:20:35 +0500

    Add files via upload

--- src/mainComponents/Modal.jsx
+++ src/mainComponents/Modal.jsx
@@ -1,40 +1,82 @@
-import React from "react";
-import { useState } from 'react';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import Select from 'react-select'
-
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import Select from 'react-select';
 import {
     MDBBtn,
     MDBModal,
     MDBModalDialog,
-    MDBRow,
-    MDBCol,
-    MDBInput,
     MDBModalContent,
     MDBModalHeader,
     MDBModalTitle,
     MDBModalBody,
-    MDBModalFooter, MDBTextArea
+    MDBModalFooter,
+    MDBRow,
+    MDBCol,
+    MDBInput,
+    MDBTextArea
 } from 'mdb-react-ui-kit';
 
-
-
 const options = [
-    { value: 'request', label: 'Request a quote/product information' },
-    { value: 'customer', label: 'Customer service/technical support' },
+    { value: 'Request a quote/product information', label: 'Request a quote/product information' },
+    { value: 'Customer service/technical support', label: 'Customer service/technical support' },
 ]
 
-
-
 function Modal({ modalName }) {
-
     const [centredModal, setCentredModal] = useState(false);
+    const [formData, setFormData] = useState({
+        inquiry_type: '',
+        from_name: '',
+        company_name: '',
+        company_address: '',
+        company_phone: '',
+        from_email: '',
+        country: '',
+        message: ''
+    });
 
     const toggleOpen = () => setCentredModal(!centredModal);
 
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_u0uxssi', 'template_phcsdew', form.current, {
+                publicKey: 'ISCQz9QQvC_DwfI9L',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    // Очистка формы после отправки
+                    setFormData({
+                        inquiry_type: '',
+                        from_name: '',
+                        company_name: '',
+                        company_address: '',
+                        company_phone: '',
+                        from_email: '',
+                        country: '',
+                        message: ''
+                    });
+                    toggleOpen(); // Закрыть модальное окно после отправки
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
     return (
-        < >
+        <>
             <MDBBtn className="text-center mt-3" onClick={toggleOpen}>{modalName}</MDBBtn>
 
             <MDBModal tabIndex='-1' open={centredModal} setOpen={setCentredModal}>
@@ -45,28 +87,84 @@ function Modal({ modalName }) {
                             <MDBBtn className='btn-close' color='none' onClick={toggleOpen}></MDBBtn>
                         </MDBModalHeader>
                         <MDBModalBody>
-                            <form>
+                            <form ref={form} onSubmit={sendEmail}>
                                 <MDBRow className='mb-4'>
                                     <MDBCol>
-                                        <Select placeholder={'Inquiry type'} options={options} />
+                                        <Select
+                                            placeholder={'Inquiry type'}
+                                            name={'inquiry_type'}
+                                            options={options}
+                                            value={formData.inquiry_type}
+                                            onChange={(selectedOption) => setFormData({ ...formData, inquiry_type: selectedOption })}
+                                        />
                                     </MDBCol>
                                     <MDBCol>
-                                        <MDBInput id='form6Example2' label='Name' />
+                                        <MDBInput
+                                            id='form6Example2'
+                                            name='from_name'
+                                            label='Name'
+                                            onChange={handleChange}
+                                            value={formData.from_name}
+                                        />
                                     </MDBCol>
                                 </MDBRow>
 
-                                <MDBInput wrapperClass='mb-4' id='form6Example3' label='Company name' />
-                                <MDBInput wrapperClass='mb-4' id='form6Example4' label='Address' />
-                                <MDBInput wrapperClass='mb-4' type='tel' id='form6Example6' label='Phone' />
-                                <MDBInput wrapperClass='mb-4' type='email' id='form6Example5' label='Email' />
-                                <MDBInput wrapperClass='mb-4' type='text' id='form6Example6' label='Country or Region' />
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    name='company_name'
+                                    id='form6Example3'
+                                    label='Company name'
+                                    onChange={handleChange}
+                                    value={formData.company_name}
+                                />
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    name='company_address'
+                                    id='form6Example4'
+                                    label='Address'
+                                    onChange={handleChange}
+                                    value={formData.company_address}
+                                />
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    name='company_phone'
+                                    type='tel'
+                                    id='form6Example6'
+                                    label='Phone'
+                                    onChange={handleChange}
+                                    value={formData.company_phone}
+                                />
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    name='from_email'
+                                    type='email'
+                                    id='form6Example5'
+                                    label='Email'
+                                    onChange={handleChange}
+                                    value={formData.from_email}
+                                />
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    name='country'
+                                    type='text'
+                                    id='form6Example6'
+                                    label='Country or Region'
+                                    onChange={handleChange}
+                                    value={formData.country}
+                                />
 
-                                <MDBTextArea label='Additional information' id='textAreaExample' rows={4} />
+                                <MDBTextArea
+                                    label='Additional information'
+                                    name='message'
+                                    id='textAreaExample'
+                                    rows={4}
+                                    onChange={handleChange}
+                                    value={formData.message}
+                                />
 
-
-
-
-
+                                <MDBBtn className='mb-4 mt-3 w-25 ' type='submit'>
+                                    Send
+                                </MDBBtn>
                             </form>
                         </MDBModalBody>
                         <MDBModalFooter>
@@ -80,8 +178,6 @@ function Modal({ modalName }) {
             </MDBModal>
         </>
     );
-
-
 }
 
 export default Modal;
--- src/mainComponents/MyCarousel.jsx
+++ src/mainComponents/MyCarousel.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import Carousel from '../assets/carousel-1.jpg'
 import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
+import carousel1 from '../assets/carousel1.png';
+import carousel2 from '../assets/carousel2.png';
+import carousel3 from '../assets/carousel3.png';
+import { Link } from 'react-router-dom';
+
 
 function MyCarousel() {
   return (
     <MDBCarousel showControls fade>
-      
+
       <MDBCarouselItem itemId={1}>
-        <img src='https://mdbootstrap.com/img/new/slides/042.jpg' className='d-block w-100 ' alt='...' />
-        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}></div>
+        <img src={carousel1} className='d-block w-100 ' alt='...' />
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)' }}>
+          <div className='col-5 text-white carousel-block1'>
+            <h1 className='display-3 text-white fw-bolder mt-2 carousel-txt'>Bacteriology</h1>
+            <p className='text-white'>Explore our diverse range of high-quality bacteriology equipment by clicking the link below.</p>
+
+            <Link to={'/catalog3'} className='btn btn-light' >See More</Link>
+          </div>
+        </div>
       </MDBCarouselItem>
 
       <MDBCarouselItem itemId={2}>
-        <img src=' https://mdbootstrap.com/img/new/slides/041.jpg' className='d-block w-100' alt='...' />
-        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}></div>
+        <img src={carousel2} className='d-block w-100' alt='...' />
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)' }}>
+          <div className='col-5 carousel-block2'>
+            <h1 className='display-3 fw-bolder carousel-txt'>Citology</h1>
+            <p>Delve into our exclusive range of cytology tools and instruments by accessing the link provided.</p>
+            <Link to={'/catalog1'}  className='btn btn-dark'>See More</Link>
+
+          </div>
+
+        </div>
       </MDBCarouselItem>
-      
+
       <MDBCarouselItem itemId={3}>
-        <img src='https://mdbootstrap.com/img/new/slides/043.jpg' className='d-block w-100' alt='...' />
-        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}></div>
+        <img src={carousel3} className='d-block w-100' alt='...' />
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)' }}>
+        <div className='col-5 carousel-block3'>
+          <h1 className='display-5 fw-bolder  carousel-txt'>Histopothology & Laborotory Supplies</h1>
+          <p>Explore our extensive range of histopathology and lab supplies.</p>
+          <Link to={'/catalog2'} className='btn btn-dark'>See More</Link>
+
+        </div>
+        </div>
       </MDBCarouselItem>
 
     </MDBCarousel>
