acf5e68ab4c49a0b16f7d0360087e0d9cfdacff6
guvanch7 <135972158+guvanch7@users.noreply.github.com>
2024-03-07 14:20:43 +0500

    Update MyCarousel.jsx

--- src/mainComponents/MyCarousel.jsx
+++ src/mainComponents/MyCarousel.jsx
@@ -30,7 +30,7 @@ function MyCarousel() {
           <img src={carousel2} className='d-block w-100' alt='...' />
           <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)' }}>
             <div className='col-5 carousel-block2'>
-              <h1 className='display-3 fw-bolder carousel-txt'>Cytology With AI Technologies</h1>
+              <h1 className='display-3 fw-bolder carousel-txt'>Cytology With AI </h1>
               {/* <p>Delve into our exclusive range of cytology tools and instruments by accessing the link provided.</p> */}
               {/* <Link to={'/catalog1'}  className='btn btn-dark'>See More</Link> */}
 
@@ -45,7 +45,7 @@ function MyCarousel() {
           <img src={carousel3} className='d-block w-100' alt='...' />
           <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)' }}>
             <div className='col-5 carousel-block3'>
-              <h1 className='display-5 fw-bolder  carousel-txt'>Histopothology & Laborotory Supplies With AI Technologies</h1>
+              <h1 className='display-5 fw-bolder  carousel-txt'>Histopothology & Laborotory Supplies With AI </h1>
               {/* <p>Explore our extensive range of histopathology and lab supplies.</p> */}
               {/* <Link to={'/catalog2'} className='btn btn-dark'>See More</Link> */}
 
